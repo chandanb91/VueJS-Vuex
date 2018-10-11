ed984f21f44b8f87e0ad8f26e597f1c246f44a9c
Chandan <chandan7791@gmail.com>
2018-10-11 15:05:50 +0530

    Why a Centralized State Alone Won't Fix it

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Vuex</h1>
+                <app-another-result></app-another-result>    
                 <app-result></app-result>
                 <hr>
                 <app-counter></app-counter>
@@ -14,11 +15,13 @@
 <script>
     import Counter from './components/Counter.vue';
     import Result from './components/Result.vue';
+    import AnotherResult from './components/AnotherResult.vue';
 
     export default {
         components: {
             appCounter: Counter,
             appResult: Result,
+            appAnotherResult: AnotherResult
         }
     }
 </script>
